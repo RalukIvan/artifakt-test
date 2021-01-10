fabf3240e87298a57d0d81247eb377d53ca31f51
Raluca Ivan <raluca.ivan87@gmail.com>
2021-01-10 14:45:22 +0100

    Add tests for EmployeeList component

---
+++ src/views/EmployeeList.spec.js
@@ -0,0 +1,76 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import { expect } from 'chai'
+import EmployeeList from './EmployeeList.vue'
+import EmployeeCard from '../components/employee/EmployeeCard.vue'
+import ErrorMessage from '../components/common/ErrorMessage.vue'
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+//create mock store for component
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueMaterial)
+
+describe('EmployeeList', () => {
+    let component
+    let actions
+    let getters
+    let store
+
+    beforeEach(() => {
+        //mock store getter for EmployeeList
+        getters = {
+            employees: () => [{
+                age: 26,
+                avatar: 'https://avatars.dicebear.com/api/male/lyrikz.svg',
+                created_at: '2020-06-06T13:37:00.000Z',
+                description: `Why don't we do it with rust? Rust is better !!`,
+                first_name: 'Nicolas',
+                id: 1,
+                last_name: 'Carquin',
+                nickname: 'Lyrikz',
+                occupation: 'back developer',
+                tags: ['Rust', 'rust', 'rust-lang'],
+                team: 'engineering',
+            }],
+            loading: () => false,
+        }
+        actions = {
+            getEmployees: () => [{
+                age: 26,
+                avatar: 'https://avatars.dicebear.com/api/male/lyrikz.svg',
+                created_at: '2020-06-06T13:37:00.000Z',
+                description: `Why don't we do it with rust? Rust is better !!`,
+                first_name: 'Nicolas',
+                id: 1,
+                last_name: 'Carquin',
+                nickname: 'Lyrikz',
+                occupation: 'back developer',
+                tags: ['Rust', 'rust', 'rust-lang'],
+                team: 'engineering',
+            }],
+        }
+        store = new Vuex.Store({
+            actions,
+            getters,
+        })
+
+        //mock Employees component
+        component = shallowMount(EmployeeList, {
+            store,
+            localVue,
+        })
+    })
+
+    it('should render EmployeeList on mount', () => {
+        expect(component.findComponent(EmployeeList).exists()).to.be.true
+    })
+
+    it('should render one EmployeeCard component', () => {
+        // check that there in one EmployeeCard component rendered
+        expect(component.findComponent(ErrorMessage).exists()).to.be.false
+        expect(component.findAllComponents(EmployeeCard).length).to.be.equal(1)
+    })
+})
